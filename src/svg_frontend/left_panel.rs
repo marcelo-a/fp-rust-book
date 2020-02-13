5447c7e364418be7710ad19410a637e098a17da0
Gabriel Luo <luogm@umich.edu>
2020-02-13 03:08:18 -0500

    Improved the left panel generation overall.
    Functionally the same, much more maintainable.

--- src/svg_frontend/left_panel.rs
+++ src/svg_frontend/left_panel.rs
@@ -5,6 +5,10 @@ use handlebars::Handlebars;
 use std::collections::{BTreeMap, HashMap};
 use serde::Serialize;
 
+struct TimelineColumnData {
+    name: String,
+    x_val: i64
+}
 
 #[derive(Serialize)]
 struct LeftPanelData {
@@ -15,23 +19,51 @@ struct LeftPanelData {
 
 #[derive(Serialize)]
 struct ResourceOwnerLabelData {
-    x_val: String,
+    x_val: i64,
     hash: String,
-    name: String,
+    name: String
 }
 
 #[derive(Serialize)]
 struct EventDotData {
-    hash: String,
-    dot_x: String,
-    dot_y: String,
+    hash: i64,
+    dot_x: i64,
+    dot_y: i64
+}
+
+#[derive(Serialize)]
+struct ArrowData {
+    x1: i64,
+    x2: i64,
+    y1: i64,
+    y2: i64
 }
 
 pub fn render_left_panel(visualization_data : &VisualizationData) -> String {
     /* Template creation */
-    let mut handlebars = Handlebars::new();
+    let mut registry = Handlebars::new();
+    prepare_registry(&mut registry);
+
+    // hash -> TimelineColumnData
+    let resource_owners_layout = compute_column_layout(visualization_data);
+
+    // render resource owner labels
+    let labels_string = render_labels_string(&resource_owners_layout, &registry);
+    let dots_string = render_dots_string(visualization_data, &resource_owners_layout, &registry);
+    let timelines_string = render_timelines_string(visualization_data, &resource_owners_layout, &registry);
+
+    let left_panel_data = LeftPanelData {
+        labels: labels_string,
+        dots: dots_string,
+        timelines: timelines_string,
+    };
+
+    registry.render("left_panel_template", &left_panel_data).unwrap()
+}
+
+fn prepare_registry(registry: &mut Handlebars) {
     // We want to preserve the inputs `as is`, and want to make no changes based on html escape.
-    handlebars.register_escape_fn(handlebars::no_escape);
+    registry.register_escape_fn(handlebars::no_escape);
 
     let left_panel_template = 
         "    <g id=\"labels\">\n{{ labels }}    </g>\n\n    \
@@ -42,59 +74,48 @@ pub fn render_left_panel(visualization_data : &VisualizationData) -> String {
     let dot_template =
         "        <use xlink:href=\"#eventDot\" data-hash=\"{{hash}}\" x=\"{{dot_x}}\" y=\"{{dot_y}}\"/>\n";
     let arrow_template = 
-        "        <polyline strokeWidth=\"3\" stroke=\"beige\" points=\"{{X1}},{{Y1}} {{X2}},{{Y2}} \" marker-end=\"url(#arrowHead)\"/>\n";
+        "        <polyline strokeWidth=\"3\" stroke=\"beige\" points=\"{{x1}},{{y1}} {{x2}},{{y2}} \" marker-end=\"url(#arrowHead)\"/>\n";
     let vertical_line_template =
-        "        <line class=\"{{line_class}}\" data-hash=\"{{hash}}\" x1=\"{{X1}}\" x2=\"{{X2}}\" y1=\"{{Y1}}\" y2=\"{{Y2}}\"/>\n";
+        "        <line class=\"{{line_class}}\" data-hash=\"{{hash}}\" x1=\"{{x1}}\" x2=\"{{x2}}\" y1=\"{{y1}}\" y2=\"{{y2}}\"/>\n";
     assert!(
-        handlebars.register_template_string("left_panel_template", left_panel_template).is_ok()
+        registry.register_template_string("left_panel_template", left_panel_template).is_ok()
     );
     assert!(
-        handlebars.register_template_string("label_template", label_template).is_ok()
+        registry.register_template_string("label_template", label_template).is_ok()
     );
     assert!(
-        handlebars.register_template_string("dot_template", dot_template).is_ok()
+        registry.register_template_string("dot_template", dot_template).is_ok()
     );
     assert!(
-        handlebars.register_template_string("arrow_template", arrow_template).is_ok()
+        registry.register_template_string("arrow_template", arrow_template).is_ok()
     );
+}
 
-    // hash -> (resource_owner name, pos)
+fn compute_column_layout<'a>(visualization_data: &'a VisualizationData) -> HashMap<&'a u64, TimelineColumnData> {
     let mut resource_owners_layout = HashMap::new();
     let mut x : i64 = -180;
     let x_space = 30;               // for every new ResourceOwner, move 30 px to the left
-    for(hash, _) in visualization_data.timelines.iter().rev() {
+    for (hash, _) in visualization_data.timelines.iter().rev() {
         let name = match visualization_data.get_name_from_hash(hash) {
             Some(_name) => _name,
             None => panic!("no matching resource owner for hash {}", hash),
         };
-        resource_owners_layout.insert(hash, (name, x));
+        resource_owners_layout.insert(hash, TimelineColumnData{ name, x_val: x });
         x = x - x_space;
     }
-
-    // render resource owner labels
-    let labels_string = render_labels_string(&resource_owners_layout, &handlebars);
-    let dots_string = render_dots_string(visualization_data, &resource_owners_layout, &handlebars);
-    let timelines_string = render_timelines_string(visualization_data, &resource_owners_layout, &handlebars);
-
-    let left_panel_data = LeftPanelData {
-        labels: labels_string,
-        dots: dots_string,
-        timelines: timelines_string,
-    };
-
-    handlebars.render("left_panel_template", &left_panel_data).unwrap()
+    resource_owners_layout
 }
 
 fn render_labels_string(
-    resource_owners_layout: &HashMap<&u64, (String, i64)>,
+    resource_owners_layout: &HashMap<&u64, TimelineColumnData>,
     registry: &Handlebars
 ) -> String {
     let mut output = String::new();
-    for(hash, (name, x_val)) in resource_owners_layout.iter() {
+    for (hash, column_data) in resource_owners_layout.iter() {
         let data = ResourceOwnerLabelData {
-            x_val: x_val.to_string(),
+            x_val: column_data.x_val,
             hash: hash.to_string(),
-            name: name.clone()
+            name: column_data.name.clone()
         };
         output.push_str(&registry.render("label_template", &data).unwrap());
     }
@@ -103,19 +124,18 @@ fn render_labels_string(
 
 fn render_dots_string(
     visualization_data: &VisualizationData,
-    resource_owners_layout: &HashMap<&u64, (String, i64)>,
+    resource_owners_layout: &HashMap<&u64, TimelineColumnData>,
     registry: &Handlebars
 ) -> String {
     let timelines = &visualization_data.timelines;
     let mut output = String::new();
     for (hash, timeline) in timelines {
         for (line_number, _) in timeline.history.iter() {
-            let mut data = BTreeMap::new();
-            let ro1_x_pos = resource_owners_layout[hash].1;
-            let ro1_y_pos = event_y_pos(line_number);
-            data.insert("hash".to_string(), *hash as i64);
-            data.insert("dot_x".to_string(), ro1_x_pos);
-            data.insert("dot_y".to_string(), ro1_y_pos);
+            let data = EventDotData {
+                hash: *hash as i64,
+                dot_x: resource_owners_layout[hash].x_val,
+                dot_y: event_y_pos(line_number)
+            };
             output.push_str(&registry.render("dot_template", &data).unwrap());
         }
     }
@@ -124,7 +144,7 @@ fn render_dots_string(
 
 fn render_timelines_string(
     visualization_data: &VisualizationData,
-    resource_owners_layout: &HashMap<&u64, (String, i64)>,
+    resource_owners_layout: &HashMap<&u64, TimelineColumnData>,
     registry: &Handlebars
 ) -> String {
     let timelines = &visualization_data.timelines;
@@ -132,21 +152,19 @@ fn render_timelines_string(
     for (hash, timeline) in timelines {
         let ro = &timeline.resource_owner;
         for (line_number, event) in timeline.history.iter() {
-            let ro1_x_pos = resource_owners_layout[hash].1;
+            let ro1_x_pos = resource_owners_layout[hash].x_val;
             let ro1_y_pos = event_y_pos(line_number);
 
             // arrows
             match event {
                 Event::Move { to : to_ro2 } => {
                     if let Some(ro2) = to_ro2 {
-                        let mut data = BTreeMap::new();
-                        let ro2_hash = & ro2.hash;
-                        let ro2_x_pos = resource_owners_layout[ro2_hash].1;
-                        let ro2_y_pos = event_y_pos(line_number);
-                        data.insert("X2".to_string(), ro2_x_pos);
-                        data.insert("Y2".to_string(), ro2_y_pos);
-                        data.insert("X1".to_string(), ro1_x_pos);
-                        data.insert("Y1".to_string(), ro1_y_pos);
+                        let data = ArrowData {
+                            x1: ro1_x_pos,
+                            y1: ro1_y_pos,
+                            x2: resource_owners_layout[&ro2.hash].x_val,
+                            y2: event_y_pos(line_number)
+                        };
                         output.push_str(&registry.render("arrow_template", &data).unwrap());
                     }
                     

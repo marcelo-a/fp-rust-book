907f29248bf1f789b53b14390282e3fe8fff3b9b
Gabriel Luo <luogm@umich.edu>
2020-02-24 16:47:09 -0500

    Book example Chap 04.01, listing 1

---
+++ examples/book_04_01_01_one_var/main.rs
@@ -0,0 +1,24 @@
+use rrt_lib::data::{Event, ExternalEvent, ResourceOwner, Visualizable, VisualizationData};
+use rrt_lib::svg_frontend::svg_generation;
+use std::collections::BTreeMap;
+// visualization of simple_lifetime/example.rs
+fn main() {
+    let s = ResourceOwner {
+        hash: 1,
+        name: String::from("s"),
+        is_mut: false,
+        // is_ref: false
+    };
+    let mut vd = VisualizationData {
+        timelines: BTreeMap::new(),
+        external_events: Vec::new(),
+    };
+    //
+    // hash s : 1
+    //
+    vd.append_external_event(2, ExternalEvent::Acquire{from: None, to: Some(s.clone())});
+    vd.append_event(&s, Event::Acquire { from: None }, &(2 as usize));
+    vd.append_event(&s, Event::GoOutOfScope, &(3 as usize));
+
+    svg_generation::render_svg(&"book_04_01_01_one_var".to_owned(), &vd);
+}

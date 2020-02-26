dcf4ebd9f611be02be56f394a216689d8b4763ce
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-02-26 17:06:17 -0500

    Added arrows corresponding to return and lend events

--- src/svg_frontend/left_panel.rs
+++ src/svg_frontend/left_panel.rs
@@ -77,16 +77,16 @@ fn prepare_registry(registry: &mut Handlebars) {
     // We want to preserve the inputs `as is`, and want to make no changes based on html escape.
     registry.register_escape_fn(handlebars::no_escape);
 
-    let left_panel_template = 
+    let left_panel_template =
         "    <g id=\"labels\">\n{{ labels }}    </g>\n\n    \
         <g id=\"events\">\n{{ dots }}    </g>\n\n    \
         <g id=\"timelines\">\n{{ timelines }}    </g>\n\n    \
         <g id=\"vertical_lines\">\n{{ vertical_lines }}    </g>\n\n";
-    let label_template = 
+    let label_template =
         "        <text class=\"code\" x=\"{{x_val}}\" y=\"80\" data-hash=\"{{hash}}\" text-anchor=\"middle\">{{name}}</text>\n";
     let dot_template =
         "        <use xlink:href=\"#eventDot\" data-hash=\"{{hash}}\" x=\"{{dot_x}}\" y=\"{{dot_y}}\"/>\n";
-    let arrow_template = 
+    let arrow_template =
         "        <polyline strokeWidth=\"2.5\" stroke=\"beige\" points=\"{{x1}},{{y1}} {{x2}},{{y2}} \" marker-end=\"url(#arrowHead)\"/>\n";
     let vertical_line_template =
         "        <line data-hash=\"{{hash}}\" class=\"{{line_class}}\" x1=\"{{x1}}\" x2=\"{{x2}}\" y1=\"{{y1}}\" y2=\"{{y2}}\"/>\n";
@@ -115,7 +115,7 @@ fn prepare_registry(registry: &mut Handlebars) {
 // Returns: a hashmap from the hash of the ResourceOwner to its Column information
 fn compute_column_layout<'a>(visualization_data: &'a VisualizationData) -> HashMap<&'a u64, TimelineColumnData> {
     let mut resource_owners_layout = HashMap::new();
-    let mut x : i64 = -40;                   // Right-most Column x-offset.    
+    let mut x : i64 = -40;                   // Right-most Column x-offset.
     let x_space = 30;                   // for every new ResourceOwner, move 30 px to the left
     for (hash, _) in visualization_data.timelines.iter().rev() {
         let name = match visualization_data.get_name_from_hash(hash) {
@@ -170,7 +170,7 @@ fn render_timelines_string(
     registry: &Handlebars
 ) -> String {
     let timelines = &visualization_data.timelines;
-    
+
     let mut output = String::new();
     for (hash, timeline) in timelines {
         let ro = timelines[hash].resource_owner.to_owned();
@@ -196,11 +196,55 @@ fn render_timelines_string(
                         }
                         output.push_str(&registry.render("arrow_template", &data).unwrap());
                     }
-                    
+
+                },
+                Event::MutableLend { to : to_ro2 } => {
+                    if let Some(ro2) = to_ro2 {
+                        let data = ArrowData {
+                            x1: ro1_x_pos,
+                            y1: ro1_y_pos,
+                            x2: resource_owners_layout[&ro2.hash].x_val-10,
+                            y2: get_y_axis_pos(line_number)
+                        };
+                        output.push_str(&registry.render("arrow_template", &data).unwrap());
+                    }
+                },
+                Event::MutableReturn { to : to_ro2 } => {
+                    if let Some(ro2) = to_ro2 {
+                        let data = ArrowData {
+                            x1: ro1_x_pos,
+                            y1: ro1_y_pos,
+                            x2: resource_owners_layout[&ro2.hash].x_val+10,
+                            y2: get_y_axis_pos(line_number)
+                        };
+                        output.push_str(&registry.render("arrow_template", &data).unwrap());
+                    }
+                },
+                Event::StaticLend { to : to_ro2 } => {
+                    if let Some(ro2) = to_ro2 {
+                        let data = ArrowData {
+                            x1: ro1_x_pos,
+                            y1: ro1_y_pos,
+                            x2: resource_owners_layout[&ro2.hash].x_val-10,
+                            y2: get_y_axis_pos(line_number)
+                        };
+                        output.push_str(&registry.render("arrow_template", &data).unwrap());
+                    }
+                },
+                Event::StaticReturn { to : to_ro2 } => {
+                    if let Some(ro2) = to_ro2 {
+                        let data = ArrowData {
+                            x1: ro1_x_pos,
+                            y1: ro1_y_pos,
+                            x2: resource_owners_layout[&ro2.hash].x_val+10,
+                            y2: get_y_axis_pos(line_number)
+                        };
+                        output.push_str(&registry.render("arrow_template", &data).unwrap());
+                    }
                 },
                 _ => (),
             };
-        } 
+        }
 
         // verticle state lines
         let states = visualization_data.get_states(hash);
@@ -239,6 +283,10 @@ fn render_timelines_string(
                     data.line_class = String::from("dotted");
                     output.push_str(&registry.render("vertical_line_template", &data).unwrap());
                 },
+                (State::ResourceReturned{ to: _ }, _) => {
+                    data.line_class = String::from("dotted");
+                    output.push_str(&registry.render("vertical_line_template", &data).unwrap());
+                },
                 (State::ResourceMoved{ move_to: _, move_at_line: _ }, true) => {
                     data.line_class = String::from("extend");
                     output.push_str(&registry.render("vertical_line_template", &data).unwrap());
@@ -258,7 +306,7 @@ fn render_vertical_lines_string(
 ) -> String {
     let timelines = &visualization_data.timelines;
     let mut output = String::new();
-    
+
     for (hash, timeline) in timelines {
         let ro_x_pos = resource_owners_layout[hash].x_val;
         for (start_line, end_line, state) in visualization_data.get_states(hash).iter(){
@@ -269,7 +317,7 @@ fn render_vertical_lines_string(
                 y2: get_y_axis_pos(end_line),
                 hash: hash.to_owned(),
                 line_class: get_vertical_line_class(state),
-                
+
             };
             output.push_str(&registry.render("vertical_line_template", &data).unwrap());
         }
@@ -286,6 +334,7 @@ fn get_vertical_line_class(state : &State) -> String {
     match state {
         State::FullPrivilege => "solid".to_string(),
         State::PartialPrivilege{borrow_count: _, borrow_to: _} => "solid".to_string(),
+        // State::RevokedPrivilege{ to: _, borrow_to: _ } => "dotted".to_string(),
         _ => "".to_string(),
     }
 }

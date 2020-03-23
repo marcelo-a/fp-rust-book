99fe19bdf9a8d630a69397594e3e1e035ca9adfe
dk <madoka@umich.edu>
2020-03-23 19:10:31 -0400

    fix &amp in 04_02_02; svg for examples up to date

--- examples/book_04_01_05_scope/annotated_source.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    {
-        let <tspan data-hash="4">s</tspan> = String::from("hello"); // s is valid from this point forward
-    
-        // do stuff with s
-    }                                  // this scope is now over, and s is no
-                                       // longer valid
-}

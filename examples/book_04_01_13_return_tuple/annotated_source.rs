19a1188e89f3198ebda005455f6fa5cf0d7bfa41
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-03-30 00:53:57 -0400

    Fixed extra semicolon in 4.1.13

--- examples/book_04_01_13_return_tuple/annotated_source.rs
+++ examples/book_04_01_13_return_tuple/annotated_source.rs
@@ -1,7 +1,7 @@
 fn main() {
     let <tspan data-hash="1">s1</tspan> = <tspan data-hash="5">String::from</tspan>("hello");
 
-    let (<tspan data-hash="6">s2</tspan>, <tspan data-hash="2">len</tspan>) = <tspan data-hash="3">calculate_length</tspan>(<tspan data-hash="1">&amp;s1</tspan>);;
+    let (<tspan data-hash="6">s2</tspan>, <tspan data-hash="2">len</tspan>) = <tspan data-hash="3">calculate_length</tspan>(<tspan data-hash="1">&amp;s1</tspan>);
     
     <tspan data-hash="9">println!</tspan>("The length of '{}' is {}.", <tspan data-hash="6">s2</tspan>, <tspan data-hash="2">len</tspan>);
 }

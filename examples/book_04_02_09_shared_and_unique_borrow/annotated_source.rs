bc84724d84cb68e996cbffca7d5055e7251337c1
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-05-28 17:17:20 -0400

    Fixed svg font styling to match book

--- examples/book_04_02_09_shared_and_unique_borrow/annotated_source.rs
+++ examples/book_04_02_09_shared_and_unique_borrow/annotated_source.rs
@@ -1,12 +1,12 @@
 fn main(){
     let mut <tspan data-hash="1">s</tspan> = <tspan class="fn" data-hash="0" hash="5">String::from</tspan>("hello");
 
-    let <tspan data-hash="2">r1</tspan> = <tspan data-hash="1">&amp;s</tspan>; // no problem
-    let <tspan data-hash="3">r2</tspan> = <tspan data-hash="1">&amp;s</tspan>; // no problem
+    let <tspan data-hash="2">r1</tspan> = <tspan data-hash="1">&amp;s</tspan>; <tspan fill="#AAA">// no problem</tspan>
+    let <tspan data-hash="3">r2</tspan> = <tspan data-hash="1">&amp;s</tspan>; <tspan fill="#AAA">// no problem</tspan>
     println!("{} and {}", <tspan data-hash="2">r1</tspan>, <tspan data-hash="3">r2</tspan>);
-    // <tspan data-hash="2">r1</tspan> and <tspan data-hash="3">r2</tspan> are no longer used after this point
+    <tspan fill="#AAA">// <tspan data-hash="2">r1</tspan> and <tspan data-hash="3">r2</tspan> are no longer used after this point</tspan>
 
-    let <tspan data-hash="4">r3</tspan> = <tspan data-hash="1">&amp;mut s</tspan>; // no problem
+    let <tspan data-hash="4">r3</tspan> = <tspan data-hash="1">&amp;mut s</tspan>; <tspan fill="#AAA">// no problem</tspan>
     println!("{}", <tspan data-hash="4">r3</tspan>);
 
 }
